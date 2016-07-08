f9241f76ea191db7a2dbbd3ffb67ba8c0f79be36
ckalagara <chandhukalagara@gmail.com>
2016-07-08 13:16:20 -0500

    Added idUser variable to UserModel for identification purpose

--- ProductRecommender/src/main/java/com/rahul/app/model/User.java
+++ ProductRecommender/src/main/java/com/rahul/app/model/User.java
@@ -8,6 +8,15 @@ public class User {
 	private String gender;
 	private String priceRange;
 	private String productCategory;
+	
+	private Long userId;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
 	public String getName() {
 		return name;
 	}
